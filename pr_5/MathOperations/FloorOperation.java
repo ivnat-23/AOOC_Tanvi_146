9882a1db084508ba4290e723a05960b18cb64769
ivnat-23 <tanvikodoli@gmail.com>
2026-03-03 11:48:00 +0530

    Add files via upload

---
+++ pr_5/MathOperations/FloorOperation.java
@@ -0,0 +1,6 @@
+package MathOperations;
+public class FloorOperation {
+    public double getFloor(double number) {
+        return Math.floor(number);
+    }
+}
---
+++ pr_5/MathOperations/RoundOperation.java
@@ -0,0 +1,6 @@
+package MathOperations;
+public class RoundOperation {
+    public long getRound(double number) {
+        return Math.round(number);
+    }
+}
